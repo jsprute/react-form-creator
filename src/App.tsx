6b94a1b61c51cfa0420785f7cddb55220d5c728d
Jason S <jesprl@hotmail.com>
2020-09-04 21:55:31 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import {JSTextBox} from './components/jstextbox';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <JSTextBox label="Test" ></JSTextBox>
+     </header>
+    </div>
+  );
+}
+
+export default App;
